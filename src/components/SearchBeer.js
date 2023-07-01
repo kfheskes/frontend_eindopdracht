2e6a25dfa24a3733230cbfb17281b33e01860a88
kfheskes <kayarneheskes@hotmail.com>
2023-07-01 14:54:14 +0200

    searchbar bijna klaar

--- src/components/SearchBeer.js
+++ src/components/SearchBeer.js
@@ -2,27 +2,28 @@ import React, {useState} from "react";
 import "./SearchBeer.css";
 import vector from "../assets/Vector.svg"
 
-function SearchBeer() {
+function SearchBeer({setDescriptionHandler}) {
     const [beerQuery, setBeerQuery] = useState('')
+
 
     function onFormSubmit (e) {
         e.preventDefault();
-
+        setDescriptionHandler(beerQuery);
     }
 
     return (
-        <form className="search-beer" >
+        <form className="search-beer" onSubmit={onFormSubmit} >
             <div className="search-img">
                 <img className="vector" alt="Vector" src={vector}/>
             <input
             type="text"
             name="search-beer"
             value={beerQuery}
+            onChange={(e) => setBeerQuery(e.target.value)}
             />
-                {/*// onChange={(e => setBeerQuery(e.target.value)}*/}
             </div>
             <div className="overlap-group">
-                <button className="button-beer-search">bier zoeken</button>
+                <button type="submit" className="button-beer-search">Search beer</button>
             </div>
         </form>
     );
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,23 +1,50 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import SearchBeer from "../../components/SearchBeer";
 import axios from "axios";
 
-function Home () {
 
-async function fetchDataBeer (){
-    try {
-        const result = await axios.get(`https://api.punkapi.com/v2/`);
-        console.log(result.data)
-    } catch (e) {
-        console.error(e);
+function Home () {
+    const [beerData, setBeerData] = useState([]);
+    const [description, setDescription] = useState('');
+    const [error, toggleError] = useState(false)
+
+    useEffect(()=> {
+    async function fetchDataBeer() {
+        toggleError(false)
+        try {
+            const result = await axios.get(`https://api.punkapi.com/v2/beers?page=1&per_page=80`);
+            console.log(result.data)
+            setBeerData(result.data);
+        } catch (e) {
+            console.error(e);
+            toggleError(true);
+        }
     }
-}
-fetchDataBeer()
+    if (description) {
+        fetchDataBeer();
+    }
+}, [description])
 
 return (
     <main className="outer-container">
         <div className="inner-container">
-        <SearchBeer/>
+        <SearchBeer setDescriptionHandler={setDescription}/>
+            {error && <span> Oeps! deze locatie bestaat niet</span>}
+            <span>
+                {Object.keys(beerData).length > 0 &&
+                    <>
+                <h2>{beerData[0].name}</h2>
+                        <p>{beerData[0].description}</p>
+                        <img src={beerData[0].image_url} alt={beerData[0].name} />
+                        <h3>Food Pairing:</h3>
+                        <ul>
+                            {beerData[0].food_pairing.map((pairing, index) => (
+                                <li key={index}>{pairing}</li>
+                            ))}
+                        </ul>
+                    </>
+                }
+            </span>
         </div>
     </main>
 

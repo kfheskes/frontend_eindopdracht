6b612e08d5d61af92fd7037779c6356ae3ca31d6
kfheskes <kayarneheskes@hotmail.com>
2023-07-29 22:29:50 +0200

    css resultBeer

--- src/components/ResultBeer/ResultBeer.js
+++ src/components/ResultBeer/ResultBeer.js
@@ -1,7 +1,7 @@
 import React, {useContext} from "react";
 import './ResultBeer.css';
 import {GlobalContext} from "../../context/GlobalState";
-
+import image from '../../assets/image.svg'
 function ResultBeer({beer}) {
     const {addFavorites, favorites} = useContext(GlobalContext);
 
@@ -14,25 +14,35 @@ function ResultBeer({beer}) {
             <div className="title-description-container">
                 <div className="title-beer">
             <h2>{beer.name}</h2>
+                    <button
+                        disabled={favoriteDisabled}
+                        onClick={() => addFavorites(beer)}> add to favourites
+                    </button>
                 </div>
                 <div className="description-beer">
+                    <h3>Description beer</h3>
             <p>{beer.description}</p>
                 </div>
             </div>
+            <div className="beer-details-container">
+                <div className="beer-details-img">
             {beer.image_url ? (
                 <img src={beer.image_url} alt={beer.name}/>
             ) : (
-                <div className="no-img"> No img available</div>)}
-            <h3>Food Pairing:</h3>
-            <ul>
+                <div>
+                <img src={image} alt="img-default"/>
+                    <h2> No img available</h2>
+                </div>)}
+                </div>
+                <div className="food-pairing">
+            <h3>Food Pairing</h3>
+            <ul className="food-pairing-list">
                 {beer.food_pairing.map((pairing) => (
                     <li key={pairing}>{pairing}</li>
                 ))}
             </ul>
-            <button
-                disabled={favoriteDisabled}
-                onClick={() => addFavorites(beer)}> add to Favorite
-            </button>
+                </div>
+            </div>
         </div>
     )
 }
